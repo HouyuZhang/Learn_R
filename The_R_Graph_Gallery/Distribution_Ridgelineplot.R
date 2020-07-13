2751b4659b2d1ef50b2a29d9f893895ec00b418a
Hughie <hughiez047@gmail.com>
2020-07-13 17:22:06 +0800

    update

---
+++ The_R_Graph_Gallery/Distribution_Ridgelineplot.R
@@ -0,0 +1,57 @@
+#https://www.r-graph-gallery.com/ridgeline-plot.html
+
+# ==============================================================================
+# 1. 
+# ==============================================================================
+library(ggridges)
+library(ggplot2)
+
+ggplot(diamonds, aes(x = price, y = cut, fill = cut)) +
+  geom_density_ridges() +
+  theme_ridges() + 
+  theme(legend.position = "none")
+
+library(dplyr)
+library(tidyr)
+library(forcats)
+
+# Load dataset from github
+data <- read.table("https://raw.githubusercontent.com/zonination/perceptions/master/probly.csv", header=TRUE, sep=",")
+data <- data %>% 
+  gather(key="text", value="value") %>%
+  mutate(text = gsub("\\.", " ",text)) %>%
+  mutate(value = round(as.numeric(value),0)) %>%
+  filter(text %in% c("Almost Certainly","Very Good Chance","We Believe","Likely","About Even", "Little Chance", "Chances Are Slight", "Almost No Chance"))
+
+data %>%
+  mutate(text = fct_reorder(text, value)) %>%
+  ggplot(aes(y=text, x=value,  fill=text)) +
+  geom_density_ridges(alpha=0.6, stat="binline", bins=20) +
+  theme_ridges() +
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  ) +
+  xlab("") +
+  ylab("Assigned Probability (%)")
+
+
+ggplot(lincoln_weather, aes(x = `Mean Temperature [F]`, y = `Month`, fill = ..x..)) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  scale_fill_viridis(name = "Temp. [F]", option = "C") +
+  labs(title = 'Temperatures in Lincoln NE in 2016') +
+  theme_ipsum() +
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  )
+
+
+
+
+
+
+
+
